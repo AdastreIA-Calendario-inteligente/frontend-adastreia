cd5e6eeb0f2b19018a1acab28ce1186bbd91be45
karoline_vieira <kdj_vieira@hotmail.com>
2025-10-29 21:45:27 -0300

    fix port

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, 
+    port: process.env.PORT || 4173, 
     proxy: {
       "/api": {
         target: "https://backend-adastreia.onrender.com",
@@ -12,5 +14,9 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
+  },
+  preview: {
+    host: true, 
+    port: process.env.PORT || 4173, 
   },
 })
