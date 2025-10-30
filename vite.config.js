0c8c7b75e5cc7d6d1661a55f257c2fbc7cea7e17
karoline_vieira <kdj_vieira@hotmail.com>
2025-10-29 21:51:17 -0300

    fix allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -18,5 +17,6 @@ export default defineConfig({
   preview: {
     host: true, 
     port: process.env.PORT || 4173, 
+    allowedHosts: ["adastreia.onrender.com"], // Adiciona o host permitido
   },
 })
